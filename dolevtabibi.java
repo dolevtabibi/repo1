3294acadc1ec2734b0aa91c6eb4c84e95b9a0611
Dolev <dolevtabibi@gmail.com>
2023-03-13 10:36:13 +0200

    3

--- dolevtabibi.java
+++ dolevtabibi.java
@@ -1,5 +1,5 @@
 class dolevtabibi {
     public static void main(String[] args) {
-        System.out.println("My name is dolev tabibi!"); 
+        System.out.println("My name is dolev tabibi!!"); 
     }
 }
